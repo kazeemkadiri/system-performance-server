b292715c43b02a51878492660154f85754479385
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2022-10-15 13:40:41 +0100

    rename build folder from react to public

--- servers.js
+++ servers.js
@@ -74,7 +74,7 @@ if (cluster.isMaster) {
 } else {
   // Note we don't use a port here because the master listens on it for us.
   let app = express();
-  app.use(express.static("./build"));
+  app.use(express.static(process.cwd() + "/public"));
 
   // Don't expose our internal server to the outside world.
   const server = app.listen(0, "localhost");
@@ -89,7 +89,7 @@ if (cluster.isMaster) {
   // server is assumed to be on localhost:6379. You don't have to
   // specify them explicitly unless you want to change them.
   // redis-cli monitor
-  io.adapter(io_redis({ host: "localhost", port: 6379 }));
+  io.adapter(io_redis({ host: "", port: 6379 }));
 
   // Here you might use Socket.IO middleware for authorization etc.
   // on connection, send the socket over to our module with socket stuff
